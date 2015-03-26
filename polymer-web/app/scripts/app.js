e2a05b4c55eff0b06eeace4438e36d0673752eea
“soonoh” <soonohjung@mytrum.com>
2015-03-27 05:22:18 +0900

    Improve menu template.

--- polymer-web/app/scripts/app.js
+++ polymer-web/app/scripts/app.js
@@ -1,28 +1,35 @@
 (function (document) {
   'use strict';
 
+  var DEFAULT_ROUTE = '/';
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Shoveling';
-  //app.route = 'info';
+  //app.appName = 'Shoveling';
+  app.pages = [
+    {name: '건설 재정 통계', route: '/'},
+    {name: '전국 시/도별 비교', route: '/compare'},
+    {name: '건설 지역 지도', route: '/map'}
+  ];
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
+
+    this.route = this.route || DEFAULT_ROUTE; // Select initial route.
   });
   
   
   function info() {
-    app.route = 'info';
+    app.route = '/';
   }
   function compare() {
-    app.route = 'compare';
+    app.route = '/compare';
   }
   function map() {
-    app.route = 'map';
+    app.route = '/map';
   }
   
   // define routes
@@ -34,7 +41,11 @@
   page({ hashbang: true });
   //page.base('');
   
-
+  app.menuItemSelected = function(e, detail, sender) {
+    if (detail.isSelected) {
+      document.querySelector('#drawer-panel').closeDrawer();
+    }
+  };
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
