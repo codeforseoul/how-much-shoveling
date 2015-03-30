f576f73e66c41ef73e6a47e4728325377fd3a762
Soonoh Jung <soonohjung@mytrum.com>
2015-03-30 18:56:12 +0900

    Load each contents asynchronously.

--- polymer-web/app/scripts/app.js
+++ polymer-web/app/scripts/app.js
@@ -6,22 +6,27 @@
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
+  var ajax, pages;
+  var cache = {};
   //app.appName = 'Shoveling';
   app.pages = [
     {
       name: '건설 재정 통계',
       route: '/',
-      desc: "건설 재정 통계"
+      desc: "건설 재정 통계",
+      url: '/info.html'
     },
     {
       name: '전국 시/도별 비교',
       route: '/compare',
-      desc: "전국 시/도별 비교"
+      desc: "전국 시/도별 비교",
+      url: '/compare.html'
     },
     {
       name: '건설 지역 지도',
       route: '/map',
-      desc: "건설 지역 지도 - 나라 장터에 체결된 계약 정보입니다"
+      desc: "건설 지역 지도 - 나라 장터에 체결된 계약 정보입니다",
+      url: '/map.html'
     }
   ];
 
@@ -30,7 +35,13 @@
   app.addEventListener('template-bound', function() {
     console.log('Our app is ready to rock!');
 
+    ajax = document.querySelector('#ajax');
+    pages = document.querySelector('#pages');
+
+    console.log('pages', pages);
+
     this.route = this.route || DEFAULT_ROUTE; // Select initial route.
+    console.log('selectedItem', app.selectedPage);
   });
 
 
@@ -54,15 +65,27 @@
   //page.base('');
 
   app.menuItemSelected = function(e, detail, sender) {
-    if (detail.isSelected) {
+    // Need to wait one rAF so <core-ajax> has it's URL set.
+    this.async(function() {
+      if (!cache[ajax.url]) {
+        ajax.go();
+      }
+
       document.querySelector('#drawer-panel').closeDrawer();
+    });
+
+    /*
+    if (detail.isSelected) {
     }
+    */
   };
 
-  template.onResponse = function(e, detail, sender) {
-    var article = detail.response.querySelector('#article-content');
+  app.ajaxLoad = function(e, detail, sender) {
+    e.preventDefault(); // prevent link navigation.
+  };
 
-    article.querySelector('.byline').remove();
+  app.onResponse = function(e, detail, sender) {
+    var article = detail.response.querySelector('#content');
 
     // Fix up image paths to not be local.
     [].forEach.call(article.querySelectorAll('img'), function(img) {
@@ -73,6 +96,7 @@
 
     cache[ajax.url] = html; // Primitive caching by URL.
 
+    console.log(pages.selectedItem);
     this.injectBoundHTML(html, pages.selectedItem.firstElementChild);
   };
 
